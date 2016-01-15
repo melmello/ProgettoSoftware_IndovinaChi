79fe1965ea4a4a8c61ca1f992b3b615c417a81ed
melmello <mellonigiulio@gmail.com>
2016-01-15 01:16:28 +0100

    1.0

--- src/sample/Client/ClientClass/ClientGameController.java
+++ src/sample/Client/ClientClass/ClientGameController.java
@@ -95,7 +95,7 @@ public class ClientGameController implements Initializable {
         System.out.println("DA FARE");
         System.out.println("PROVA");
         System.out.println("NOVITA");
-
+        System.out.println("FINE");
     }
 
     public void clientWantsToQuery(){
