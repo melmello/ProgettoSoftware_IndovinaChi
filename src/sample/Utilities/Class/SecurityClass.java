80503d2516c3507f643ee6904643decf8fc49e78
melmello <mellonigiulio@gmail.com>
2016-01-21 18:08:47 +0100

    1.0.1

--- src/sample/Utilities/Class/SecurityClass.java
+++ src/sample/Utilities/Class/SecurityClass.java
@@ -2,10 +2,6 @@ package sample.Utilities.Class;
 
 public final class SecurityClass {
 
-    public static final String mySQLUsername = "root";
-    public static final String mySQLPassword = "RTrododentro14";
-    public static final String googleMail = "giulio.development.melloni@gmail.com";
-    public static final String googlePassword = "Develop94";
-    public static final String googleTrueMail = "mellonigiulio@gmail.com";
+    //not public
 
 }
